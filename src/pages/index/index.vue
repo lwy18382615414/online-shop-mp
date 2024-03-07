00ca662be926b10dde97b3c17e2cb85ba3ce3dad
李文屹 <18382615414@163.com>
2024-03-07 16:58:26 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view class="index"> 123 </view>
+</template>
+
+<style lang="scss">
+//
+</style>
---
+++ src/stores/modules/member.ts
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+// 定义 Store
+export const useMemberStore = defineStore(
+  'member',
+  () => {
+    // 会员信息
+    const profile = ref<any>()
+
+    // 保存会员信息，登录时使用
+    const setProfile = (val: any) => {
+      console.log(val)
+      profile.value = val
+    }
+
+    // 清理会员信息，退出时使用
+    const clearProfile = () => {
+      profile.value = undefined
+    }
+
+    // 记得 return
+    return {
+      profile,
+      setProfile,
+      clearProfile,
+    }
+  },
+  // TODO: 持久化
+  {
+    // 网页端
+    // persist: true,
+
+    // 小程序端配置
+    persist: {
+      storage: {
+        getItem(key: any) {
+          return uni.getStorageSync(key)
+        },
+        setItem(key: string, value: any) {
+          uni.setStorageSync(key, value)
+        },
+      },
+    },
+  },
+)
